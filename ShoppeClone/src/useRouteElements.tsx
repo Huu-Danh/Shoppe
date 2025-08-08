2b8d46f62e1906a6d3731b4e7ca5ed5aaa072ad1
Huu Danh <nguyentanhuudanhit>
2025-08-08 14:55:27 +0700

    code ui header thuoc mainlayout

--- ShoppeClone/src/useRouteElements.tsx
+++ ShoppeClone/src/useRouteElements.tsx
@@ -3,11 +3,19 @@ import ProductList from './pages/ProductList'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import RegisterLayout from './layouts/RegisterLayout'
+import MainLayout from './layouts/MainLayuout'
 
 export default function useRouteElements() {
   return (
     <Routes>
-      <Route path='/' element={<ProductList />} />
+      <Route
+        path='/'
+        element={
+          <MainLayout>
+            <ProductList />
+          </MainLayout>
+        }
+      />
       <Route
         path='/login'
         element={
