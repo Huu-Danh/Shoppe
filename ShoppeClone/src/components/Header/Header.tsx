d96cb898437364e5e9e438aae5bcbc084820d4a0
Huu Danh <nguyentanhuudanhit>
2025-08-09 16:45:45 +0700

    tao component popover

--- ShoppeClone/src/components/Header/Header.tsx
+++ ShoppeClone/src/components/Header/Header.tsx
@@ -1,31 +1,21 @@
-import { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FloatingPortal, useFloating, arrow, shift, offset } from '@floating-ui/react'
-import { AnimatePresence, motion } from 'motion/react'
+import Popover from '../Popover'
 export default function Header() {
-  const [isOpen, setOpen] = useState(true)
-  const arrowRef = useRef(null)
-  const { x, y, refs, strategy, middlewareData } = useFloating({
-    middleware: [
-      offset(6),
-      shift(),
-      arrow({
-        element: arrowRef
-      })
-    ]
-  })
-  const showPopover = () => {
-    setOpen(true)
-  }
-  const hidePopover = () => {
-    setOpen(false)
-  }
-
   return (
     <div className='pb-5 pt-2 bg-[linear-gradient(-180deg,_#f53d2d,_#f63)] text-white'>
       <div className='container'>
-        <div className='flex justify-end ' onMouseEnter={showPopover} onMouseLeave={hidePopover}>
-          <div className='flex items-center py-1 hover:text-gray-300 cursor-pointer' ref={refs.setReference}>
+        <div className='flex justify-end '>
+          <Popover
+            className='flex items-center py-1 hover:text-gray-300 cursor-pointer'
+            renderPopover={
+              <div className='bg-white relative shadow-md rounded-sm border border-white'>
+                <div className='flex flex-col py-2 px-3'>
+                  <button className='py-2 px-3 hover:text-orange'>Tiếng Việt</button>
+                  <button className='py-2 px-3 hover:text-orange mt-2'>English</button>
+                </div>
+              </div>
+            }
+          >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -52,45 +42,29 @@ export default function Header() {
             >
               <path strokeLinecap='round' strokeLinejoin='round' d='m19.5 8.25-7.5 7.5-7.5-7.5' />
             </svg>
-          </div>
-          <FloatingPortal>
-            <AnimatePresence>
-              {isOpen && (
-                <motion.div
-                  ref={refs.setFloating}
-                  style={{
-                    position: strategy,
-                    top: y ?? 0,
-                    left: x ?? 0,
-                    width: 'max-content',
-                    transformOrigin: `${middlewareData.arrow?.x}px top`
-                  }}
-                  initial={{ opacity: 0, transform: 'scale(0)' }}
-                  animate={{ opacity: 1, transform: 'scale(1)' }}
-                  exit={{ opacity: 0, transform: 'scale(0)' }}
-                  transition={{ duration: 0.2 }}
+          </Popover>
+          <Popover
+            className='flex items-center py-1 hover:text-gray-300 cursor-pointer ml-6'
+            renderPopover={
+              <div className='bg-white relative shadow-md rounded-sm border border-white'>
+                <Link
+                  to='/'
+                  className='block py-2 px-3 hover:bg-gray-100 bg-white hover:text-cyan-500 w-full text-left'
                 >
-                  <div
-                    ref={arrowRef}
-                    className='border-x-transparent border-t-transparent border-b-white border-[11px] absolute translate-y-[_-90%]'
-                    style={{
-                      position: 'absolute',
-                      left: middlewareData.arrow?.x,
-                      top: middlewareData.arrow?.y
-                    }}
-                  />
-                  <div className='bg-white relative shadow-md rounded-sm border border-white'>
-                    <div className='flex flex-col py-2 px-3'>
-                      <button className='py-2 px-3 hover:text-orange'>Tiếng Việt</button>
-                      <button className='py-2 px-3 hover:text-orange mt-2'>English</button>
-                    </div>
-                  </div>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </FloatingPortal>
-
-          <div className='flex items-center py-1 hover:text-gray-300 cursor-pointer ml-6'>
+                  Tài khoản của tôi
+                </Link>
+                <Link
+                  to='/'
+                  className='block py-2 px-3 hover:bg-gray-100 bg-white hover:text-cyan-500 w-full text-left'
+                >
+                  Đơn mua
+                </Link>
+                <button className='block py-2 px-3 hover:bg-gray-100 bg-white hover:text-cyan-500 w-full text-left'>
+                  Đăng xuất
+                </button>
+              </div>
+            }
+          >
             <div className='w-5 h-5 mr-2 flex-shrink-0'>
               <img
                 src='https://down-vn.img.susercontent.com/file/vn-11134233-7ra0g-m9s4bp5hje3ed8_tn'
@@ -99,9 +73,10 @@ export default function Header() {
               />
             </div>
             <div className='mr-3'>NguyenTanHuuDanh</div>
-          </div>
-        </div>
+          </Popover>
 
+          <div className='flex items-center py-1 hover:text-gray-300 cursor-pointer ml-6'></div>
+        </div>
         <div className='grid grid-cols-12 gap-4 mt-4 items-end'>
           <Link to='/' className='col-span-2'>
             <svg viewBox='0 0 192 65' className='ml-3 h-11 lg:h-11 fill-white'>
---
+++ ShoppeClone/src/components/Popover/Popover.tsx
@@ -0,0 +1,65 @@
+import { useRef, useState } from 'react'
+import { FloatingPortal, useFloating, arrow, shift, offset } from '@floating-ui/react'
+import { AnimatePresence, motion } from 'motion/react'
+
+interface Props {
+  children: React.ReactNode
+  renderPopover: React.ReactNode
+  className?: string
+}
+export default function Popover({ children, className, renderPopover }: Props) {
+  const [isOpen, setOpen] = useState(false)
+  const arrowRef = useRef(null)
+  const { x, y, refs, strategy, middlewareData } = useFloating({
+    middleware: [
+      offset(6),
+      shift(),
+      arrow({
+        element: arrowRef
+      })
+    ]
+  })
+  const showPopover = () => {
+    setOpen(true)
+  }
+  const hidePopover = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div className={className} ref={refs.setReference} onMouseEnter={showPopover} onMouseLeave={hidePopover}>
+      {children}
+      <FloatingPortal>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              ref={refs.setFloating}
+              style={{
+                position: strategy,
+                top: y ?? 0,
+                left: x ?? 0,
+                width: 'max-content',
+                transformOrigin: `${middlewareData.arrow?.x}px top`
+              }}
+              initial={{ opacity: 0, transform: 'scale(0)' }}
+              animate={{ opacity: 1, transform: 'scale(1)' }}
+              exit={{ opacity: 0, transform: 'scale(0)' }}
+              transition={{ duration: 0.2 }}
+            >
+              <div
+                ref={arrowRef}
+                className='border-x-transparent border-t-transparent border-b-white border-[11px] absolute translate-y-[_-90%]'
+                style={{
+                  position: 'absolute',
+                  left: middlewareData.arrow?.x,
+                  top: middlewareData.arrow?.y
+                }}
+              />
+              {renderPopover}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </FloatingPortal>
+    </div>
+  )
+}
