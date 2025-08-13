cfc87ae9009565a678f2f2c997a0014596e9708d
Huu Danh <nguyentanhuudanhit>
2025-08-13 21:45:34 +0700

    Code ui asidefilter

--- ShoppeClone/src/components/Input/Input.tsx
+++ ShoppeClone/src/components/Input/Input.tsx
@@ -1,24 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import type { InputHTMLAttributes } from 'react'
 import type { UseFormRegister } from 'react-hook-form'
 
-interface Props {
-  type: React.HTMLInputTypeAttribute
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  placeholder?: string
-  className: string
-  name: string
-  register: UseFormRegister<any>
+  classNameInput?: string
+  classNameError?: string
+  register?: UseFormRegister<any>
 }
-export default function Input({ type, errorMessage, placeholder, className, name, register }: Props) {
+export default function Input({
+  type,
+  errorMessage,
+  placeholder,
+  className,
+  name,
+  register,
+  classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
+  classNameError = 'mt-1 text-red-600 min-h-[1.25rem] text-sm'
+}: Props) {
+  const registerResult = register && name ? register(name) : {}
   return (
     <div className={className}>
-      <input
-        type={type}
-        className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-        placeholder={placeholder}
-        {...register(name)}
-      />
-      <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errorMessage}</div>
+      <input type={type} className={classNameInput} placeholder={placeholder} {...registerResult} />
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
---
+++ ShoppeClone/src/pages/ProductList/SortProductlist/SortProductlist.tsx
@@ -0,0 +1,3 @@
+export default function SortProductlist() {
+  return <div>SortProductlist</div>
+}
