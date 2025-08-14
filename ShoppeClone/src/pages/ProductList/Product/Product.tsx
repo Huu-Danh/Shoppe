05e12641b2c5aafc56606dac544c59c2018efbeb
Huu Danh <nguyentanhuudanhit>
2025-08-14 14:28:52 +0700

    code ui product componet

--- ShoppeClone/src/pages/ProductList/Product/Product.tsx
+++ ShoppeClone/src/pages/ProductList/Product/Product.tsx
@@ -4,7 +4,7 @@ import path from 'src/constants/path'
 export default function Product() {
   return (
     <Link to={path.home}>
-      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.0625rem] hover:shadow-md duration-100 transition-transform'>
+      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
         <div className='w-full pt-[100%] relative'>
           <img
             src='https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lupvwpc810cyf9_tn'
@@ -13,7 +13,7 @@ export default function Product() {
           />
         </div>
         <div className='p-2 overflow-hiden'>
-          <div className='min-h-[1.75rem] line-clamp-2 text-sm'>
+          <div className='min-h-[2rem] line-clamp-2 text-xs'>
             Thắt Lưng Nam Khóa Tự Động Cao Cấp Mặt Xoay Chính Hãng , Dây Nịt Nam Phong Cách Hàn Quốc
           </div>
           <div className='flex items-center mt-3'>
@@ -23,7 +23,59 @@ export default function Product() {
             </div>
             <div className='text-orange truncate'>
               <span className='text-xs'>₫</span>
-              <span>5.000</span>
+              <span>2.000</span>
+            </div>
+          </div>
+          <div className='mt-3 flex items-center justify-end'>
+            <div className='flex items-center'>
+              <div className='relative'>
+                <div className='absolute left-0 top-0 h-full overflow-hidden' style={{ width: '50%' }}>
+                  <svg viewBox='0 0 9.5 8' className='w-3 h-3 fill-yellow-300'>
+                    <defs>
+                      <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
+                        <stop offset={0} stopColor='#ffca11' />
+                        <stop offset={1} stopColor='#ffad27' />
+                      </linearGradient>
+                      <polygon
+                        id='ratingStar'
+                        points='14.910357 6.35294118 12.4209136 7.66171903 12.896355 4.88968305 10.8823529 2.92651626 13.6656353 2.52208166 14.910357 0 16.1550787 2.52208166 18.9383611 2.92651626 16.924359 4.88968305 17.3998004 7.66171903'
+                      />
+                    </defs>
+                    <g fill='url(#ratingStarGradient)' fillRule='evenodd' stroke='none' strokeWidth={1}>
+                      <g transform='translate(-876 -1270)'>
+                        <g transform='translate(155 992)'>
+                          <g transform='translate(600 29)'>
+                            <g transform='translate(10 239)'>
+                              <g transform='translate(101 10)'>
+                                <use stroke='#ffa727' strokeWidth='.5' xlinkHref='#ratingStar' />
+                              </g>
+                            </g>
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+                <svg viewBox='0 0 30 30' className='w-3 h-3 text-gray-300 fill-current'>
+                  <defs>
+                    <linearGradient id='star__hollow' x1='50%' x2='50%' y1='0%' y2='99.0177926%'>
+                      <stop offset='0%' stopColor='#FFD211' />
+                      <stop offset='100%' stopColor='#FFAD27' />
+                    </linearGradient>
+                  </defs>
+                  <path
+                    fill='none'
+                    fillRule='evenodd'
+                    stroke='url(#star__hollow)'
+                    strokeWidth={2}
+                    d='M23.226809 28.390899l-1.543364-9.5505903 6.600997-6.8291523-9.116272-1.4059447-4.01304-8.63019038-4.013041 8.63019038-9.116271 1.4059447 6.600997 6.8291523-1.543364 9.5505903 8.071679-4.5038874 8.071679 4.5038874z'
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className='ml-2 text-sm'>
+              <span>5.6k</span>
+              <span className='ml-1'>Đã bán</span>
             </div>
           </div>
         </div>
